d2794b7257f4600c0a84bbf91970a44b96b206c4
Gordon <gordnaman@gmail.com>
2023-01-15 21:10:53 +0800

    can you hear me?

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
@@ -11,13 +11,8 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <h1>can you hear me?</h1>
       </main>
-
-      <Footer />
     </div>
-  )
+  );
 }
